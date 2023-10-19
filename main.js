5a12c0edc4320df8e8295d5b21751be169f4709c
nnasko <nnasskooo@gmail.com>
2023-10-19 17:16:12 +0100

    webtimeapi

---
+++ main.js
@@ -0,0 +1,79 @@
+document.getElementById('citySelect').disabled=true;
+    
+    // Function to make a fetch GET request
+    function fetchGet(url) {
+        return fetch(url)
+            .then(response => response.json())
+            .catch(error => console.error(error));
+    }
+
+    // Populate the continent dropdown
+    const continentSelect = document.getElementById("continentSelect");
+
+    fetchGet("http://worldtimeapi.org/api/timezones")
+        .then(timezones => {
+            const continents = {};
+
+            // Extract continents from time zone names
+            timezones.forEach(timezone => {
+                const parts = timezone.split("/");
+                if (parts.length > 1) {
+                    const continent = parts[0];
+                    continents[continent] = true;
+                }
+            });
+
+            for (const continent in continents) {
+                const option = document.createElement("option");
+                option.value = continent;
+                option.textContent = continent;
+                continentSelect.appendChild(option);
+            }
+        });
+
+    // When the continent selection changes
+    continentSelect.addEventListener("change", () => {
+        const selectedContinent = continentSelect.value;
+        const citySelect = document.getElementById("citySelect");
+
+        // Make a request to get the cities for the selected continent
+        fetchGet(`http://worldtimeapi.org/api/timezone/${selectedContinent}`)
+            .then(data => {
+                // Clear the city dropdown
+                citySelect.innerHTML = '<option value="" class="rounded">Select City</option>';
+                // Populate the city dropdown with cities from the response data
+                data.forEach(city => {
+                    const option = document.createElement("option");
+                    option.value = city;
+                    option.className = "dropdowns"
+                    // Extract and display only the city name
+                    const cityParts = city.split('/');
+                    option.textContent = cityParts[cityParts.length - 1];
+                    citySelect.appendChild(option);
+                });
+            });
+    });
+
+    // When the city selection changes
+    const citySelect = document.getElementById("citySelect");
+    citySelect.addEventListener("change", () => {
+        const selectedCity = citySelect.value;
+        const timeResult = document.getElementById("timeResult");
+
+        // Make a request to get the time for the selected city
+        fetchGet(`http://worldtimeapi.org/api/timezone/${selectedCity}`)
+            .then(data => {
+                // Display the time information
+                let result = data.datetime.toString();
+                let time = result.substring(11,16)
+
+                timeResult.textContent = `Time in ${selectedCity}: ${time}`;
+            });
+    });
+function check(){
+    if(document.getElementById('continentSelect').value!="")
+        document.getElementById('citySelect').disabled=false;
+    else
+        document.getElementById('citySelect').disabled=true;
+}
+    
